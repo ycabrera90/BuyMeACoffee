7adb580bab2517264624c4fd5806cb8c8cdd4056
Yosniel Cabrera <yosniel.ch@gmail.com>
2024-06-24 09:02:49 -0300

    NA: the return a success path in payment were fixed

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,24 +1,22 @@
-const dlocal = require("../services/dlocalPayment");
+const dlocal = require("../services/dlocalPayment")
 
 exports.buyACoffee = (_, res) => {
-  res.render("buy-me-coffee", { pageTitle: "Buy me a coffee 🤓" });
-};
+	res.render("buy-me-coffee", { pageTitle: "Buy me a coffee 🤓" })
+}
 
 exports.goToPay = (req, res) => {
-  const { amount, currency } = req.body;
-  const success_url =
-    req.protocol + "://" + req.get("host") + "/payment/buy-me-a-coffee";
-  const back_url =
-    req.protocol + "://" + req.get("host") + "/payment/buy-me-a-coffee";
+	const { amount, currency } = req.body
+	const success_url = req.protocol + "://" + req.get("host")
+	const back_url = req.protocol + "://" + req.get("host")
 
-  dlocal.payment(
-    { amount, currency, success_url, back_url },
-    ({ response, error }) => {
-      if (error) {
-        res.send({ code: response.code, message: response.message });
-      } else {
-        res.redirect(response.data.redirect_url);
-      }
-    }
-  );
-};
+	dlocal.payment(
+		{ amount, currency, success_url, back_url },
+		({ response, error }) => {
+			if (error) {
+				res.send({ code: response.code, message: response.message })
+			} else {
+				res.redirect(response.data.redirect_url)
+			}
+		}
+	)
+}
